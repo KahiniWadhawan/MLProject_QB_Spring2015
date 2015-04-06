b62e493b5cc3034f81f92b93a49ff7c1d84245fe
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2015-04-05 20:10:24 -0600

    changed some methods in DataManipulation

--- Data Extraction/main.py
+++ Data Extraction/main.py
@@ -1,40 +1,50 @@
+import os
 from csv import DictReader
-import math
 
-# given two coordinates, this function returns a speed 
-speed = lambda (x1,y1),(x2,y2): math.sqrt(((x2-x1)**2+(y2-y1)**2))
-# given a list of coordinates, this function returns the total length of a trip
-trip_length = lambda x_y: sum([speed(x_y[e],x_y[e+1]) for e in xrange(len(x_y)) if e < len(x_y)-1])
+folder_path = "../../drivers"
 
 class DataManipulation(object):
-    def __init__(self, filepath):
-        self.filepath = filepath    
-        
-    def data_import(self):
-        # input: the location of .csv file as a string. E.g."drivers/1/1.csv"
-        # output: a list of tuple, [(x1,y1),(x2,y2),....]
-        for driver in drivers:
-            for trip in driver:
-                data = list(DictReader(open(self.filepath,"r")))
-                x_y = [(float(e["x"]),float(e["y"])) for e in data]
-        return x_y
 
-    def data_dump(self):
-        
-        
-    def main():
-        filename = "drivers/1/1.csv" # change this as you need to
-        x_y = data_import(filename)
-        print speed(x_y[0],x_y[1])
-        print trip_length(x_y)
-     
-    def data_plot():
-        # output: a list of tuple, [(x1,y1),(x2,y2),....]
-        data = list(DictReader(open(filename,"r")))
-        x = [(float(e["x"])) for e in data]
-        y = [(float(e["y"])) for e in data]
+    def __init__(self, folder_path="../../drivers"):
+        # input: the path of the data folder
+        self.folder_path = folder_path   
+
+    def data_driver(self,name="1"):
+        # input: the name of a driver (string)
+        # output: a dictionary whose key is a trip and value is a list of coordinates (tuple)
+        #         so overall 200 trips
+
+        self.trips = {}
+        path = self.folder_path+"/"+str(name)+"/"
+
+        for f in os.listdir(path):
+            data = list(DictReader(open(path+f,"r")))
+            x_y = [(float(e["x"]),float(e["y"])) for e in data]
+            self.trips[f[:-4]] = x_y
+
+        return self.trips
+
+    def data_all(self):
+        # this method imports all data so may take some time 
+        # output -> dict of dict: {drive1:{trip1:[(x1,y1),(x2,y2),...],trip2:[...]},driver2:...}
+        self.all = {}
+
+        for driver in os.listdir():
+            self.all[driver] = self.data_driver(name=driver)
+
+        return self.all
+   
+    def plot_helper(self,trip="1"):
+        driver = self.data_driver(name="1")
+        x = [x for x,y in driver[trip]]
+        y = [y for x,y in driver[trip]]
         return x,y
-        	
+
+def main():
+    Data = DataManipulation(folder_path)
+    driver_trip =  Data.data_driver("1")
+    x_y = driver_trip["1"] # notice that when you access a dict, key is a string ("1", "2", etc.)
+    print x_y  	
 
 
 if __name__ == "__main__":
