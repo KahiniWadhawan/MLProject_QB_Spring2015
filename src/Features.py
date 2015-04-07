02c27097666722ebd9b28f2820d253cba8630d16
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2015-04-07 17:24:22 -0600

    genre feature added

--- src/Features.py
+++ src/Features.py
@@ -1,5 +1,17 @@
+import numpy as np
 
-number_of genre = 11
+"""
+Note Pad
+
+>>> a = np.array([1,2,3])
+>>> b = np.array([4,5,6])
+>>> np.concatenate((a,b))
+array([1, 2, 3, 4, 5, 6])
+
+"""
+genres = ['Earth Science', 'Biology', 'Literature', 'Astronomy',
+		  'Fine Arts', 'Other', 'Social Studies', 'Mathematics',
+		  'Chemistry', 'Physics', 'History']
 
 class FeatureExtractor:
 	"""
@@ -11,8 +23,22 @@ class FeatureExtractor:
 	regression tasks since both may need different features.
 	"""
 	def __init__(self,question):
-		self.questions = question
-	
+		self.question = question
+
+	def genre(self):
+		"""
+		this method returns a numpy array with only one entry = 1
+		whose index corresponds to a position of a given question's genre
+		in a list, genres.
+		"""
+		vector = np.zeros(len(genres))
+		vector[genres.index(self.question["genre"])] = 1
+		return vector
+
 	def extract():
 		# this method should return a numpy array as a X vector
+		# concatenating feature vectors from other methods using
+		# np.concatenate()
 		pass
+
+
