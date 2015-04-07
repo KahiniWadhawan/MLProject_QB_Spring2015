babf60a84566f57342a679a386b807ad986279a2
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2015-04-07 10:14:37 -0600

    main.py added.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from csv import DictReader
+
+folder_path = "../data"
+
+def data_import(path):
+	"""
+	input: a path to the .csv file you want to import
+	output: a list of dict whose keys are the names of columns
+	"""
+	return list(DictReader(open(path,"r")))
+
+if __name__ == "__main__":
+	train = data_import(folder_path+"/train.csv")
+	for e in train:
+		print e
