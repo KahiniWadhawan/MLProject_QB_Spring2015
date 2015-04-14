1bd3da4d95a10287e734895b884e50cb50a2f468
Kansuke Ikehara <kansuke.ikehara@colorado.edu>
2015-04-13 21:25:01 -0600

    modified SuperModel.py

---
+++ src/Models/SuperModel.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from CorrectnessModel import CorrectnessModel
+from PositionModel import PositionModel
+
+
+class SuperModel:
+	def __init__(self):
+		self.correctness = CorrectnessModel()
+		self.position = PositionModel()
+
+	def fit_co(self,X,Y):
+		self.correctness.fit(X,Y)
+
+	def fit_pos(self,X,Y):
+		self.position.fit(X,Y)
+
+	def predict_correct(self,X,pos):
+		return self.correctness.predict(X)
+
+	def predict_pos(self,X):
+		return self.position.predict(X)
+
+	def predict(self,X_co,X_pos):
+		pos = self.predict_pos(X_pos)
+		sign = self.predict_correct(X_co,pos)
+		return pos*sign
+
+
+if __name__ == "__main__":
