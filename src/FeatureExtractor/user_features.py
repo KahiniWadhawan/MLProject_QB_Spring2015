5fde4c480c9a6638383a28ea3f15a432799945ac
FireBal <kahini.wadhwan@gmail.com>
2015-04-13 19:40:25 -0600

    added two user features

--- src/FeatureExtractor/user_features.py
+++ src/FeatureExtractor/user_features.py
@@ -6,8 +6,34 @@ from question import Question
 from parent_feature_extractor import FeatureExtractor
  
 class UserFeatureExtractor(FeatureExtractor):
-    def __init__(self, user_id, qid):
+    def __init__(self, user_id):
         self.user = User(user_id)
-        self.question = Question(qid)
+        #self.question = Question(qid)
         self.user_id = self.user.user_id
+
+
+    def user_category_correctness_ratio(self,user_id,category):
+	
+	conn = sqlite3.connect('../../../data/quizbowl_buzz.db')
+        cur = conn.cursor()
+	query = "select (sum(CASE WHEN position > 0 THEN 1.0 ELSE 0.0 END)/count(t.question)) as cor_ratio from questions q JOIN train t ON q.id = t.question where usage != ? and category = ? and user = ? order by cor_ratio DESC "
+        c = cur.execute(query,("test",category,user_id))
+        cor_ratio = c.fetchall()[0][0]
+       	print "cor_ratio :: ", cor_ratio
         
+	return cor_ratio	
+        
+
+    def user_category_avg_buzz(self,user_id,category):
+	
+	conn = sqlite3.connect('../../../data/quizbowl_buzz.db')
+        cur = conn.cursor()
+	query = "select avg(abs(position)) as avg_buzz from questions q JOIN train t ON q.id = t.question where usage != ? and category = ? and user = ? group by user order by avg_buzz DESC"
+        c = cur.execute(query,("test",category,user_id))
+	avg_buzz = c.fetchall()[0][0]
+       	print "avg_buzz :: ", avg_buzz
+        
+	return avg_buzz	
+        
+
+
