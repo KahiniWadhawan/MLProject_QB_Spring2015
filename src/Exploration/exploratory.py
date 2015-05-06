891609ac0a6bcd7b05fdeee703a8a856a0bb5859
FireBal <kahini.wadhwan@gmail.com>
2015-05-05 21:17:09 -0600

    corrected queries

--- src/Exploration/exploratory.py
+++ src/Exploration/exploratory.py
@@ -7,21 +7,25 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 class Exploration(object):
     def __init__(self):
-        os.chdir('../../data')
-        self.conn = sqlite3.connect('quizbowl_buzz3.db')
+        os.chdir('../../../data')
+        self.conn = sqlite3.connect('quizbowl_buzz.db')
         self.cur = self.conn.cursor()
-        self.pp = PdfPages('all_plots.pdf')
+        self.pp = PdfPages('all_plots_corrected.pdf')
     
     def ques_per_cat(self):
-        query = "select category, count(*) as count from questions where usage != 'test' group by category order by count DESC "
+        #query = "select category, count(*) as count from questions q JOIN train t ON q.id = t.question group by category order by count DESC "
+	query = "select category, count(*) as count  from questions where id in (select distinct question from train) group by category order by count DESC "
         c = self.cur.execute(query,)
         data = c.fetchall()
         X = [z[0] for z in data]
         Y = [z[1] for z in data]
         plt.bar(range(len(Y)), Y, align='center')
         plt.xticks(range(len(X)), X, size='small', rotation='vertical')
-        plt.title("Questions per Category")
-        #plt.show()
+        plt.title("Questions per Category", size = 'small')
+        #plt.figure(figsize=(1.5,1.5))
+	#plt.show()
+	#print plt.figure.get_size_inches()
+	#plt.savefig('plt.png',dpi=100)
         self.pp.savefig()
         plt.close()
         #self.pp.close()
@@ -30,24 +34,24 @@ class Exploration(object):
         print "\n\n"
 
     def user_per_cat(self):
-        query = "select category, count(distinct user) as count from questions q JOIN train t ON q.id = t.question where usage != 'test'  group by category order by count DESC "
+        query = "select category, count(distinct user) as count from questions q JOIN train t ON q.id = t.question group by category order by count DESC "
         c = self.cur.execute(query,)
         data = c.fetchall()
         X = [z[0] for z in data]
         Y = [z[1] for z in data]
         plt.bar(range(len(Y)), Y, align='center')
         plt.xticks(range(len(X)), X, size='small', rotation='vertical')
-        plt.title("Unique users per Category")
+        plt.title("Users per Category")
         #plt.show()
         plt.savefig(self.pp, format = 'pdf')
         #print c.fetchall()
-        self.pp.savefig()
+        #self.pp.savefig()
         plt.close()
         #self.pp.close()
         print "\n\n"
         
     def cat_avg_buzz_pos(self):
-        query = "select category, avg(abs(position)) as avg_buzz from questions q JOIN train t ON q.id = t.question where usage != 'test'  group by category order by avg_buzz DESC "
+        query = "select category, avg(abs(position)) as avg_buzz from questions q JOIN train t ON q.id = t.question group by category order by avg_buzz DESC "
         c = self.cur.execute(query,)
         data = c.fetchall()
         X = [z[0] for z in data]
@@ -58,30 +62,31 @@ class Exploration(object):
         #plt.show()
         plt.savefig(self.pp, format = 'pdf')
         #print c.fetchall()
-        self.pp.savefig()
+        #self.pp.savefig()
         plt.close()
         #self.pp.close()
         print "\n\n"
         
     def user_avg_buzz_pos(self):
-        query = "select user, avg(abs(position)) as avg_buzz from questions q JOIN train t ON q.id = t.question where usage != 'test'  group by user order by avg_buzz DESC"
+        query = "select user, avg(abs(position)) as avg_buzz from questions q JOIN train t ON q.id = t.question group by user order by avg_buzz DESC"
         c = self.cur.execute(query,)
         data = c.fetchall()
         X = [z[0] for z in data]
         Y = [z[1] for z in data]
         plt.bar(range(len(Y)), Y, align='center')
         plt.xticks(range(len(X)), X, size='small', rotation='vertical')
+	#plt.xlabel("Users")
         plt.title("Avg. Buzz position per User")
         #plt.show()
         plt.savefig(self.pp, format = 'pdf')
         #print c.fetchall()
-        self.pp.savefig()
+        #self.pp.savefig()
         plt.close()
         #self.pp.close()
         print "\n\n"
         
     def cat_correctness_ratio(self):
-        query = "select category, (sum(CASE WHEN position > 0 THEN 1.0 ELSE 0.0 END)/count(t.question)) as cor_ratio from questions q JOIN train t ON q.id = t.question where usage != 'test'  group by category order by cor_ratio DESC "
+        query = "select category, (sum(CASE WHEN position > 0 THEN 1.0 ELSE 0.0 END)/count(t.question)) as cor_ratio from questions q JOIN train t ON q.id = t.question group by category order by cor_ratio DESC "
         c = self.cur.execute(query,)
         data = c.fetchall()
         X = [z[0] for z in data]
@@ -92,7 +97,7 @@ class Exploration(object):
         #plt.show()
         plt.savefig(self.pp, format = 'pdf')
         #print c.fetchall()
-        self.pp.savefig()
+        #self.pp.savefig()
         self.pp.close()
         plt.close()
         print "\n\n"
